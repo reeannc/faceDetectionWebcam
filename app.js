49cb988df27db61cc090c5782c6580089d42dcef
reeannc <119835137+reeannc@users.noreply.github.com>
2023-08-31 16:32:30 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const video = document.getElementById("video");
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("/weights"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/weights"),
+]).then(startWebcam);
+
+function startWebcam() {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: false,
+    })
+    .then((stream) => {
+      video.srcObject = stream;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+  faceapi.matchDimensions(canvas, { height: video.height, width: video.width });
+
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks();
+
+    const resizedDetections = faceapi.resizeResults(detections, {
+      height: video.height,
+      width: video.width,
+    });
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, detections);
+    faceapi.draw.drawFaceLandmarks(canvas, detections);
+
+    console.log(detections);
+  }, 100);
+});
